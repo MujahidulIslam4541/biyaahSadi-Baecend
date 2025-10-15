38fe041b79e36e68828e94374f534fd6c81ac18c
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-10-15 14:24:16 +0600

    Add Friend model with schema and export functionality

---
+++ src/models/friends.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const friendsSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    profileId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "blocked"],
+      default: "pending",
+    },
+  },
+  {
+    timestamps: true, // automatically adds createdAt & updatedAt
+  }
+);
+
+const Friend = mongoose.model("Friend", friendsSchema);
+
+module.exports = Friend;
